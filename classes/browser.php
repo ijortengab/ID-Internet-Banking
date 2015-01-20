df5fcd82fd43495797873217cbef3a4b65a6e162
ijortengab <m_roji28@yahoo.com>
2015-01-20 15:47:47 +0700

    update method browse and create new some property

--- classes/browser.php
+++ classes/browser.php
@@ -11,7 +11,19 @@
  */
 class browser {
 
+  // The main URL.
+  private $url;
+
+  // The variable that stored result of function parse_url().
+  // Use for quickreference.
+  public $parse_url;
+
+  // Current working directory, a place for save anything.
   private $cwd;
+
+  // We are using PHP's Stream as default.
+  // If you prefer using curl, set as TRUE.
+  private $curl = FALSE;
 
   function __construct($url = NULL) {
     // Set url.
@@ -19,8 +31,16 @@ class browser {
       $this->setUrl($url);
     }
     // Buat object Cookie Storage
-    // Buat object 
-    
+    // Buat object
+
+  }
+
+  public function setUrl($url) {
+
+  }
+
+  public function getUrl($url) {
+    return $this->url;
   }
 
   public function setCwd($dir) {
@@ -42,6 +62,16 @@ class browser {
     return $this->cwd;
   }
 
+  public function curl($switch = TRUE) {
+    if ($switch && function_exists('curl_init')) {
+      return $this->curl = TRUE;
+    }
+    else {
+      return $this->curl = FALSE;
+    }
+  }
+
+  // Main function.
   public function browse() {
     // Current working directory is required, if not exists
     // set working directory with current directory.
@@ -49,7 +79,32 @@ class browser {
       $this->setCwd(__DIR__);
     }
 
+    $method = $this->curl ? 'curl_request' : 'drupal_http_request';
+    switch ($method) {
+      case 'curl_request':
+        $result = $this->{$method}();
+        break;
+
+      case 'drupal_http_request':
+      default:
+        $result = $this->{$method}();
+        break;
+    }
+
+
+
 
     echo 'b';
   }
+
+  // Modified of function drupal_http_request in Drupal 7.
+  protected function drupal_http_request() {
+    echo 'drupal_http_request()';
+  }
+
+  //
+  protected function curl_request() {
+    echo 'curl_request()';
+  }
+
 }
